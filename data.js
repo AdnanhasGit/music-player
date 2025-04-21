34c411a62e95d20c85043774a1d19a9845246814
Adnanhasgit <getadnan995@gmail.com>
2025-04-21 23:47:03 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,15 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'framed - altyn(MP3_160K).mp3',
+        artist: 'artist 1',
+        cover: 'images',
+    },
+
+    {
+        name: 'song 2',
+        path: 'G-Eazy _ Halsey - Him _ I (Lyrics)(MP3_160K).mp3',
+        artist: 'artist 2',
+        cover: 'ímage 2',
+    }
+]
